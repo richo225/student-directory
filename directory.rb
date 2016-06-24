0d13de9e1b37848d04380d123b169bef0f1411b2
richo225 <richo225@gmail.com>
2016-06-24 14:28:18 +0100

    Ex8 Add third cohort to be printed. Need to refactor with proc.

--- directory.rb
+++ directory.rb
@@ -65,6 +65,17 @@ def print students
       puts ""
     end
   end
+
+  #third cohort
+  students.each do |student|
+    if student[:cohort] == existing_cohorts[2]
+      puts "   Name:".ljust(11) + "-".center(8) + "#{student[:name]}"
+      puts "   Cohort:".ljust(11) + "-".center(8) + "#{student[:cohort]}"
+      puts "   Country:".ljust(11) + "-".center(8) + "#{student[:country]}"
+      puts "   Hobby:".ljust(11) + "-".center(8) + "#{student[:hobby]}"
+      puts ""
+    end
+  end
 end
 
 #add a footer method that prints student count
