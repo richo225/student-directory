b0d8664211c8ca290f72a89f6c2253b4781369fb
richo225 <richo225@gmail.com>
2016-06-28 13:22:38 +0100

    Add comments to quine.rb

--- quine.rb
+++ quine.rb
@@ -1,9 +1,14 @@
+#get the ABSOLUTE path of the file
 path = File.expand_path(File.dirname(__FILE__))
 
+#get the name of the file
 file_name = __FILE__
 
+#open the file using its name and absolute path
 File.open("#{path}/#{file_name}", "r") do |file|
+  #read through each line of the file and print them
   file.each_line do |line|
     puts line
   end
+  #block causes file to close automatically
 end
